355d53684a9d3becb112aa2ea7753f886b135b85
InhaKr <inkr3891@gmail.com>
2025-09-06 08:30:53 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const spans = document.querySelectorAll('.population');
+
+
+  const value = Array.from(spans).map((span) => span.textContent);
+  let r = 0;
+
+  for (let i = 0; i < value.length; i++) {
+    // console.log(typeof(value[i]));
+    r += +value[i].replace(/,/g, '');
+  }
+
+  const total = document.querySelector('.total-population');
+
+  total.textContent = r.toLocaleString('en-US');
+
+  const average = document.querySelector('.average-population');
+
+  average.textContent = Math.floor(r / value.length).toLocaleString('en-US');
+});
